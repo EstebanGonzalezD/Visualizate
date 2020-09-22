265592e39d266804fbdfa941a88905cdabf6d02d
Esteban <esgduque147@gmail.com>
2020-09-22 09:19:44 -0500

    Mail

--- src/java/Servlets/ServletRegistro.java
+++ src/java/Servlets/ServletRegistro.java
@@ -11,8 +11,18 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Date;
+
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
+//Librerias para el correo.
+import javax.mail.Message;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.util.Properties;
+
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -126,8 +136,9 @@ public class ServletRegistro extends HttpServlet {
                 }
 
                 if (sw) {
-                                        RequestDispatcher despachador = request.getRequestDispatcher("login/sign_in.jsp");
+                    RequestDispatcher despachador = request.getRequestDispatcher("login/sign_in.jsp");
                     despachador.forward(request, response);
+                    enviarCorreo(ParamCorreo);
                 } else {
                     PrintWriter out = response.getWriter();
                     out.println("Si estas viendo este mensaje es por que algo salio mal, no se pudo completar tu solicitud. " + nombre + ParamContraseña + ParamCorreo + ParamFecha + ParamGenero + ParamUsuario);
@@ -163,6 +174,69 @@ public class ServletRegistro extends HttpServlet {
             processRequest(request, response);
         }
 
+    }
+    
+    public boolean enviarCorreo(String para){
+        String de = "visualizateqa@gmail.com";
+        String clave = "yLt2*62s";
+        String asunto = "¡Bienvenido a Visualízate!";
+        String mensaje = "Te has registrado correctamente. Ahora puedes acceder a todos los beneficios personalizados.";
+        boolean enviado = false;
+     
+            try{
+            
+                String host = "smtp.gmail.com";
+                
+                Properties prop = System.getProperties();
+                
+                prop.put("mail.smtp.starttls.enable","true");
+                prop.put("mail.smtp.host", host);
+                prop.put("mail.smtp.user",de);
+                prop.put("mail.smtp.password", clave);
+                prop.put("mail.smtp.port",587);
+                prop.put("mail.smtp.auth","true");
+                
+                Session sesion = Session.getDefaultInstance(prop,null);
+                
+                MimeMessage message = new MimeMessage(sesion);
+                
+                message.setFrom(new InternetAddress(de));
+                
+                /*
+                    
+                    NOTA: para enviar correo electronico masivo
+                
+                    InternetAddress[] direcciones = new InternetAddress[para.length];
+                    for(int i=0;i<para.length;i++){
+                        direcciones[i] = new InternetAddress(para[i]);
+                    }
+                
+                    for(int i=0;i<direcciones.length;i++){
+                        message.addRecipient(Message.RecipientType.TO, direcciones[i]);
+                    }
+                
+                */
+                
+                message.setRecipient(Message.RecipientType.TO, new InternetAddress(para));
+                
+                message.setSubject(asunto);
+                message.setText(mensaje);
+                
+                Transport transport = sesion.getTransport("smtp");
+                
+                transport.connect(host,de,clave);
+                
+                transport.sendMessage(message, message.getAllRecipients());
+                
+                transport.close();
+                
+                enviado = true;
+                
+            }catch(Exception e){
+                e.printStackTrace();
+            }
+        
+        return enviado;
     }
 
     @Override
